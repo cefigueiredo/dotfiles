adba1baf6306d2614814020013797b62ea17dba3
Carlos Figueiredo <lecaduco@proton.me>
2022-07-26 01:59:30 -0400

    Configure help and icons for nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,1 +1,30 @@
-require("nvim-tree").setup()
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+
+if not status_ok then
+  return
+end
+
+nvim_tree.setup({
+  view = {
+    mappings = {
+      list = {
+        { key = {"g?", "?"}, action = "toggle_help" }
+      }
+    }
+  },
+  renderer= {
+    icons = {
+      glyphs = {
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        }
+      }
+    }
+  }
+})
