0e8476752edbd8e0ad241070592a6c1bbd322d7f
Carlos Figueiredo <lecaduco@proton.me>
2022-07-27 20:21:59 -0400

    Config LSP for elixir and refactor custom lsp settings

--- lua/user/lsp_config.lua
+++ lua/user/lsp_config.lua
@@ -33,32 +33,38 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 local servers = {
   ['solargraph'] = {},
   ['sumneko_lua'] = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        }
       }
     }
+  },
+  ['elixirls'] = {
+    cmd = {"/home/cefig/dev/elixir-ls-1.13/language_server.sh"}
   }
 }
 
-for lsp, settings in pairs(servers) do
+for lsp, custom_settings in pairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
     flags = { debounce_text_changes = 150, },
     capabilities = capabilities,
-    settings = settings
+    settings = custom_settings.settings,
+    cmd = custom_settings.cmd
   }
 end
