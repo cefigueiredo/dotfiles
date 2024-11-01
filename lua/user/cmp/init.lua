8531c81c6dde6beaa19c2cea3d9368c074a240bb
Carlos Figueiredo <cfigueiredo@wrapbook.com>
2024-11-01 16:15:58 -0400

    change cmp keymaps to avoid conflict with tmux

--- lua/user/cmp/init.lua
+++ lua/user/cmp/init.lua
@@ -15,8 +15,8 @@ cmp.setup({
       documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<A-j>'] = cmp.mapping.scroll_docs(-4),
+    ['<A-k>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete({}),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
